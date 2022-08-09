980afc5116c3c56f9e2a6e1fc6047c023db99eaf
aranthana <aru2191@gmail.com>
2022-08-09 19:04:42 +1000

    Elliptive curve

--- elliptive-curve/elliptive-curve/Program.cs
+++ elliptive-curve/elliptive-curve/Program.cs
@@ -86,9 +86,75 @@ namespace elliptivecurve{
             double oldX = x;
             double m = ((3 * x * x) + 3) / (2 * y);
             x = m * m - 2 * x;
-            y = - Math.Round(m, 5) * (Math.Round(x,5) - oldX) - y;
+            y = - m * (x - oldX) - y;
             WriteLine("Doubled point : " + x + "," + y);
         }
+
+        private static void AddPointsFiniteField(ref double x_r, ref double y_r, double x, double y)
+        {
+            int prime = 1613;
+            WriteLine("Points to be added : " + x_r + "," + y_r + " + " + x + "," + y);
+            double m = ((y_r - y) * ModInversCalculation((int)(x_r - x),prime)) % prime;
+            x_r = ((m * m) - (x_r + x)) % prime;
+            y_r = (m * (x - x_r) - y) % prime;
+            WriteLine("Added point and generated : " + x_r + " ," + y_r);
+        }
+
+        private static void DoubleThePointFiniteFiled(ref double x, ref double y)
+        {
+            int prime = 1613;
+            WriteLine("Points to be doubled : " + x + "," + y);
+            double oldX = x;
+            double m =(((3 * x * x) + 3) * ModInversCalculation((int)(2 * y), prime)) % prime ;
+            x = (m * m - 2 * x) % prime;
+            y = (m * (oldX-x) - y) % prime;
+            WriteLine("Doubled point : " + x + "," + y);
+        }
+
+          /*  Method to calculate mod inverse. */
+        private static int ModInversCalculation(int a, int m)
+        {
+
+            int g = Gcd(a, m);
+            if (g != 1)
+                WriteLine("Inverse doesn't exist");
+            else
+            {
+                // If a and m are relatively
+                // prime, then modulo inverse
+                // is a^(m-2) mode m
+                WriteLine(
+                    "Modular multiplicative inverse is "+Power(a, m - 2, m)
+                    );
+            }
+            return Power(a, m - 2, m);
+        }
+
+        // To compute x^m-2 under
+        // modulo m
+        static int Power(int x, int y, int m)
+        {
+            if (y == 0)
+                return 1;
+    
+            int p = Power(x, y / 2, m) % m;
+            p = (p * p) % m;
+            if (y % 2 == 0)
+                return p;
+            else
+                return (x * p) % m;
+        }
+
+        // Function to return
+        // gcd of a and b
+        static int Gcd(int a, int b)
+        {
+            if (a == 0)
+                return b;
+            return Gcd(b % a, a);
+        }
+
+
     }
 
 
